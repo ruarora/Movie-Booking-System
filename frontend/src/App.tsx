3b0c449f323db0d02ca4fcf787e98805940036ca
Punit Soni <punit_soni@epam.com>
2023-03-17 12:32:10 +0530

    - routing updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <Routes>
 
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
         <Route path="/home" element={<Home />} />
