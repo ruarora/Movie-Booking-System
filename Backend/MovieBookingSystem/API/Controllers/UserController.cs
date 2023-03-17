12768c4d975eae1cafa65488a26cdc4e17f096ab
Somnath Roy <somnath_roy@epam.com>
2023-03-17 12:27:16 +0530

    login and register api

--- Backend/MovieBookingSystem/API/Controllers/UserController.cs
+++ Backend/MovieBookingSystem/API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,6 +8,49 @@ namespace API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        public static List<User> UserList = new List<User>()
+        {
+            new User(){
+                Name = "Admin",
+                Email = "admin@admin.com",
+                Password = "admin123" // can be hashed later on. 
+            },
+            new User(){
+            Name = "User",
+                Email = "user@user.com",
+                Password = "user123" // can be hashed later on. 
+            }
+        };
+        public UserController()
+        {
 
+        }
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(User userDetails){
+            if(userDetails == null)
+                return BadRequest("Invalid Request");
+
+            var isUserExists = UserList.Exists(x => x.Email == userDetails.Email && x.Password == userDetails.Password);
+
+            return Ok(isUserExists);
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (user == null)
+                return BadRequest("Invalid Request");
+
+            var isUserExists = UserList.Exists(x => x.Email == user.Email);
+
+            if (isUserExists)
+            {
+                return BadRequest("User Already Exists!");
+            }
+
+            UserList.Add(user);
+
+            return Ok();
+        }
     }
 }
